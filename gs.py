e3bcd86257fdf64904b8ea601f0f55e1fff77e73
Jormq99 <99856936+Jormq99@users.noreply.github.com>
2023-10-01 18:18:51 -0600

    Update gs.py

--- gs.py
+++ gs.py
@@ -1,30 +1,35 @@
-from time import sleep
-from threading import Thread
-
 import gi
 
 gi.require_version("Gst", "1.0")
+from gi.repository import Gst
 
-from gi.repository import Gst, GLib
+def main():
+    # Crear el pipeline
 
+    pipeline_video = Gst.Pipeline()
 
-Gst.init()
+    # Agregar el elemento filesrc
 
-main_loop = GLib.MainLoop()
-thread = Thread(target=main_loop.run)
-thread.start()
+    filesrc = Gst.ElementFactory.make("filesrc")
+    filesrc.set_property("location", "coco.mp4")
+    pipeline_video.add(filesrc)
 
-pipeline_video = Gst.parse_launch("udpsrc multicast-group=224.1.1.1 auto-multicast=true port=5001 ! application/x-rtp  ! rtph264depay ! h264parse ! queue ! decodebin ! videoconvert ! autovideosink")
-pipeline_audio = Gst.parse_launch("udpsrc multicast-group=224.1.1.1 auto-multicast=true port=5000 ! rawaudioparse use-sink-caps=false format=pcm pcm-format=s16le sample-rate=16000 num-channels=1 ! queue ! audioconvert ! audioresample ! autoaudiosink")
-pipeline_video.set_state(Gst.State.PLAYING)
+    # Agregar el elemento windowsink
 
-pipeline_audio.set_state(Gst.State.PLAYING)
+    windowsink = Gst.ElementFactory.make("windowsink")
+    windowsink.set_property("x", 100)
+    windowsink.set_property("y", 100)
+    windowsink.set_property("width", 640)
+    windowsink.set_property("height", 480)
+    pipeline_video.add(windowsink)
 
-try:
-    while True:
-        sleep(0.1)
-except KeyboardInterrupt:
-    pass
-pipeline_video.set_state(Gst.State.NULL)
-pipeline_audio.set_state(Gst.State.NULL)
-main_loop.quit()
+    # Iniciar el pipeline
+
+    pipeline_video.set_state(Gst.State.PLAYING)
+
+    # Esperar a que el pipeline termine
+
+    pipeline_video.get_state(Gst.ClockTime.TIME_NONE)
+
+if __name__ == "__main__":
+    main()
